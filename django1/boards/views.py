a9f3da0916984f416ccc49020f5afeb706126251
joyc <joyc@live.jp>
2019-01-29 00:57:08 +0900

    add new_topic func and page

--- django1/boards/views.py
+++ django1/boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Topic, Post
 
 
 def home(request):
@@ -20,6 +21,32 @@ def board_topics(request, pk):
     except Board.DoesNotExist:
         raise Http404
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    """新增主题"""
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() # TODO 临时使用一个账号作为登录用户
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)    # TODO 跳转到新增主题页面
+
+    return render(request, 'new_topic.html', {'board': board})
 
 
 def about(request):
--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -21,6 +21,7 @@ from boards import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^about/$', views.about, name='about'),
     # url(r'^(?P<username>[\w.@+-]+)/$', views.user_profile, name='user_profile'),
     url(r'^admin/', admin.site.urls),
